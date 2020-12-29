38c30c83cfbbe38dffd1c65489e252bdcc4d5d3d
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-29 13:37:17 -0500

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Hung-Chun Tsai on 2020-12-29.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
